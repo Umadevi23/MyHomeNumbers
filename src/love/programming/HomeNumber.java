9f21dd95fe2c4cd66884dcd67e9dd5b0ba6a95fb
Umadevi23 <uma_gorakati@yahoo.co.in>
2019-03-07 13:55:24 +0000

    MyHomeNumbers

---
+++ src/love/programming/HomeNumber.java
@@ -0,0 +1,15 @@
+package love.programming;
+
+public class HomeNumber {
+
+    int a = 10;
+    int b = 25;
+
+
+    public static void main (String[] args) {
+        int x = 10;
+        int y = 25;
+        int c = x + y;
+        System.out.println(" value of c ="+ c );
+    }
+}
